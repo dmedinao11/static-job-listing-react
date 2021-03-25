16db5f7468defdf16c0205816970e792e0d91e4f
Daniel Medina <danielmedina1119@gmail.com>
2021-03-25 13:00:47 -0500

    Segundo release

--- src/features/jobsList/components/job/Job.tsx
+++ src/features/jobsList/components/job/Job.tsx
@@ -7,43 +7,38 @@ import JobTag from "../jobTag/JobTag";
 import "./Job.css";
 
 const Job = ({ job }: { job: IJob }) => {
-  const imagesRelativePath = "../../../../images/";
-  return (
-    <div className={`job ${job.featured ? "job--featured" : ""}`}>
-      <Card>
-        <div className="job__container">
-          <img
-            className="job__logo"
-            src={imagesRelativePath + job.logo}
-            alt="logo"
-          />
-          <div className="job__description">
-            <div className="job__tags">
-              <h1 className="job__enterprise">{job.company}</h1>
-              {job.new && <div className="job__chip job__chip--new">NEW!</div>}
-              {job.featured && (
-                <span className="job__chip job__chip--featured">FEATURED</span>
-              )}
-            </div>
-            <h1 className="job__title">{job.position}</h1>
-            <div className="job__info">
-              {`${job.postedAt} · ${job.contract} · ${job.location}`}
-            </div>
-          </div>
-          <div className="job__filterTags">
-            <JobTag tag={job.role} />
-            <JobTag tag={job.level} />
-            {job.tools.map((tool, index) => (
-              <JobTag key={index} tag={tool} />
-            ))}
-            {job.languages.map((language, index) => (
-              <JobTag key={index} tag={language} />
-            ))}
-          </div>
-        </div>
-      </Card>
-    </div>
-  );
+	return (
+		<div className={`job ${job.featured ? "job--featured" : ""}`}>
+			<Card>
+				<div className="job__container">
+					<img className="job__logo" src={job.logo} alt="logo" />
+					<div className="job__description">
+						<div className="job__tags">
+							<h1 className="job__enterprise">{job.company}</h1>
+							{job.new && <div className="job__chip job__chip--new">NEW!</div>}
+							{job.featured && (
+								<span className="job__chip job__chip--featured">FEATURED</span>
+							)}
+						</div>
+						<h1 className="job__title">{job.position}</h1>
+						<div className="job__info">
+							{`${job.postedAt} · ${job.contract} · ${job.location}`}
+						</div>
+					</div>
+					<div className="job__filterTags">
+						<JobTag tag={job.role} />
+						<JobTag tag={job.level} />
+						{job.tools.map((tool, index) => (
+							<JobTag key={index} tag={tool} />
+						))}
+						{job.languages.map((language, index) => (
+							<JobTag key={index} tag={language} />
+						))}
+					</div>
+				</div>
+			</Card>
+		</div>
+	);
 };
 
 export default Job;
--- src/features/jobsList/jobsListReducer.ts
+++ src/features/jobsList/jobsListReducer.ts
@@ -2,157 +2,168 @@ import { createReducer } from "@reduxjs/toolkit";
 import { Contract, IJob, Level, Role } from "../../app/jobList/job.interface";
 import { RootState } from "../../app/store";
 
+import photosnap from "../../images/photosnap.svg";
+import manage from "../../images/manage.svg";
+import account from "../../images/account.svg";
+import myhome from "../../images/myhome.svg";
+import loopStudios from "../../images/loop-studios.svg";
+import faceit from "../../images/faceit.svg";
+import shortly from "../../images/shortly.svg";
+import insure from "../../images/insure.svg";
+import eyecamCo from "../../images/eyecam-co.svg";
+import theAirFilterCompany from "../../images/the-air-filter-company.svg";
+
 const initialState: IJob[] = [
-  {
-    id: 1,
-    company: "Photosnap",
-    logo: "photosnap.svg",
-    new: true,
-    featured: true,
-    position: "Senior Frontend Developer",
-    role: "Frontend" as Role,
-    level: "Senior" as Level,
-    postedAt: "1d ago",
-    contract: "Full Time" as Contract,
-    location: "USA Only",
-    languages: ["HTML", "CSS", "JavaScript"],
-    tools: []
-  },
-  {
-    id: 2,
-    company: "Manage",
-    logo: "manage.svg",
-    new: true,
-    featured: true,
-    position: "Fullstack Developer",
-    role: "Fullstack" as Role,
-    level: "Midweight" as Level,
-    postedAt: "1d ago",
-    contract: "Part Time" as Contract,
-    location: "Remote",
-    languages: ["Python"],
-    tools: ["React"]
-  },
-  {
-    id: 3,
-    company: "Account",
-    logo: "account.svg",
-    new: true,
-    featured: false,
-    position: "Junior Frontend Developer",
-    role: "Frontend" as Role,
-    level: "Junior" as Level,
-    postedAt: "2d ago",
-    contract: "Part Time" as Contract,
-    location: "USA Only",
-    languages: ["JavaScript"],
-    tools: ["React", "Sass"]
-  },
-  {
-    id: 4,
-    company: "MyHome",
-    logo: "myhome.svg",
-    new: false,
-    featured: false,
-    position: "Junior Frontend Developer",
-    role: "Frontend" as Role,
-    level: "Junior" as Level,
-    postedAt: "5d ago",
-    contract: "Contract" as Contract,
-    location: "USA Only",
-    languages: ["CSS", "JavaScript"],
-    tools: []
-  },
-  {
-    id: 5,
-    company: "Loop Studios",
-    logo: "loop-studios.svg",
-    new: false,
-    featured: false,
-    position: "Software Engineer",
-    role: "Fullstack" as Role,
-    level: "Midweight" as Level,
-    postedAt: "1w ago",
-    contract: "Full Time" as Contract,
-    location: "Worldwide",
-    languages: ["JavaScript"],
-    tools: ["Ruby", "Sass"]
-  },
-  {
-    id: 6,
-    company: "FaceIt",
-    logo: "faceit.svg",
-    new: false,
-    featured: false,
-    position: "Junior Backend Developer",
-    role: "Backend" as Role,
-    level: "Junior" as Level,
-    postedAt: "2w ago",
-    contract: "Full Time" as Contract,
-    location: "UK Only",
-    languages: ["Ruby"],
-    tools: ["RoR"]
-  },
-  {
-    id: 7,
-    company: "Shortly",
-    logo: "shortly.svg",
-    new: false,
-    featured: false,
-    position: "Junior Developer",
-    role: "Frontend" as Role,
-    level: "Junior" as Level,
-    postedAt: "2w ago",
-    contract: "Full Time" as Contract,
-    location: "Worldwide",
-    languages: ["HTML", "JavaScript"],
-    tools: ["Sass"]
-  },
-  {
-    id: 8,
-    company: "Insure",
-    logo: "insure.svg",
-    new: false,
-    featured: false,
-    position: "Junior Frontend Developer",
-    role: "Frontend" as Role,
-    level: "Junior" as Level,
-    postedAt: "2w ago",
-    contract: "Full Time" as Contract,
-    location: "USA Only",
-    languages: ["JavaScript"],
-    tools: ["Vue", "Sass"]
-  },
-  {
-    id: 9,
-    company: "Eyecam Co.",
-    logo: "eyecam-co.svg",
-    new: false,
-    featured: false,
-    position: "Full Stack Engineer",
-    role: "Fullstack" as Role,
-    level: "Midweight" as Level,
-    postedAt: "3w ago",
-    contract: "Full Time" as Contract,
-    location: "Worldwide",
-    languages: ["JavaScript", "Python"],
-    tools: ["Django"]
-  },
-  {
-    id: 10,
-    company: "The Air Filter Company",
-    logo: "the-air-filter-company.svg",
-    new: false,
-    featured: false,
-    position: "Front-end Dev",
-    role: "Frontend" as Role,
-    level: "Junior" as Level,
-    postedAt: "1mo ago",
-    contract: "Part Time" as Contract,
-    location: "Worldwide",
-    languages: ["JavaScript"],
-    tools: ["React", "Sass"]
-  }
+	{
+		id: 1,
+		company: "Photosnap",
+		logo: photosnap,
+		new: true,
+		featured: true,
+		position: "Senior Frontend Developer",
+		role: "Frontend" as Role,
+		level: "Senior" as Level,
+		postedAt: "1d ago",
+		contract: "Full Time" as Contract,
+		location: "USA Only",
+		languages: ["HTML", "CSS", "JavaScript"],
+		tools: []
+	},
+	{
+		id: 2,
+		company: "Manage",
+		logo: manage,
+		new: true,
+		featured: true,
+		position: "Fullstack Developer",
+		role: "Fullstack" as Role,
+		level: "Midweight" as Level,
+		postedAt: "1d ago",
+		contract: "Part Time" as Contract,
+		location: "Remote",
+		languages: ["Python"],
+		tools: ["React"]
+	},
+	{
+		id: 3,
+		company: "Account",
+		logo: account,
+		new: true,
+		featured: false,
+		position: "Junior Frontend Developer",
+		role: "Frontend" as Role,
+		level: "Junior" as Level,
+		postedAt: "2d ago",
+		contract: "Part Time" as Contract,
+		location: "USA Only",
+		languages: ["JavaScript"],
+		tools: ["React", "Sass"]
+	},
+	{
+		id: 4,
+		company: "MyHome",
+		logo: myhome,
+		new: false,
+		featured: false,
+		position: "Junior Frontend Developer",
+		role: "Frontend" as Role,
+		level: "Junior" as Level,
+		postedAt: "5d ago",
+		contract: "Contract" as Contract,
+		location: "USA Only",
+		languages: ["CSS", "JavaScript"],
+		tools: []
+	},
+	{
+		id: 5,
+		company: "Loop Studios",
+		logo: loopStudios,
+		new: false,
+		featured: false,
+		position: "Software Engineer",
+		role: "Fullstack" as Role,
+		level: "Midweight" as Level,
+		postedAt: "1w ago",
+		contract: "Full Time" as Contract,
+		location: "Worldwide",
+		languages: ["JavaScript"],
+		tools: ["Ruby", "Sass"]
+	},
+	{
+		id: 6,
+		company: "FaceIt",
+		logo: faceit,
+		new: false,
+		featured: false,
+		position: "Junior Backend Developer",
+		role: "Backend" as Role,
+		level: "Junior" as Level,
+		postedAt: "2w ago",
+		contract: "Full Time" as Contract,
+		location: "UK Only",
+		languages: ["Ruby"],
+		tools: ["RoR"]
+	},
+	{
+		id: 7,
+		company: "Shortly",
+		logo: shortly,
+		new: false,
+		featured: false,
+		position: "Junior Developer",
+		role: "Frontend" as Role,
+		level: "Junior" as Level,
+		postedAt: "2w ago",
+		contract: "Full Time" as Contract,
+		location: "Worldwide",
+		languages: ["HTML", "JavaScript"],
+		tools: ["Sass"]
+	},
+	{
+		id: 8,
+		company: "Insure",
+		logo: insure,
+		new: false,
+		featured: false,
+		position: "Junior Frontend Developer",
+		role: "Frontend" as Role,
+		level: "Junior" as Level,
+		postedAt: "2w ago",
+		contract: "Full Time" as Contract,
+		location: "USA Only",
+		languages: ["JavaScript"],
+		tools: ["Vue", "Sass"]
+	},
+	{
+		id: 9,
+		company: "Eyecam Co.",
+		logo: eyecamCo,
+		new: false,
+		featured: false,
+		position: "Full Stack Engineer",
+		role: "Fullstack" as Role,
+		level: "Midweight" as Level,
+		postedAt: "3w ago",
+		contract: "Full Time" as Contract,
+		location: "Worldwide",
+		languages: ["JavaScript", "Python"],
+		tools: ["Django"]
+	},
+	{
+		id: 10,
+		company: "The Air Filter Company",
+		logo: theAirFilterCompany,
+		new: false,
+		featured: false,
+		position: "Front-end Dev",
+		role: "Frontend" as Role,
+		level: "Junior" as Level,
+		postedAt: "1mo ago",
+		contract: "Part Time" as Contract,
+		location: "Worldwide",
+		languages: ["JavaScript"],
+		tools: ["React", "Sass"]
+	}
 ];
 
 const jobsReducer = createReducer(initialState, (state) => state);
@@ -160,13 +171,13 @@ const jobsReducer = createReducer(initialState, (state) => state);
 export default jobsReducer;
 
 export const selectFilteredJobs = (state: RootState) => {
-  return state.filters.tags.reduce((previus: IJob[], current: string) => {
-    return previus.filter(
-      (job: IJob) =>
-        job.role === current ||
-        job.level === current ||
-        job.tools.includes(current) ||
-        job.languages.includes(current)
-    );
-  }, state.jobs);
+	return state.filters.tags.reduce((previus: IJob[], current: string) => {
+		return previus.filter(
+			(job: IJob) =>
+				job.role === current ||
+				job.level === current ||
+				job.tools.includes(current) ||
+				job.languages.includes(current)
+		);
+	}, state.jobs);
 };
